2e885a4f055e85b910809aeb73488b4b0a8b81ee
Siddiqur Rahman <mrahman.vf.ic.bd@gmail.com>
2016-09-07 16:12:18 +0600

    student class is created.

--- ResultManagement/src/Student.java
+++ ResultManagement/src/Student.java
@@ -1,4 +1,52 @@
 
 public class Student {
-
+	private String name;
+	private String roll;
+	private String address;
+	private String clsName;
+	private String fatherName;
+	private String motherName;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getRoll() {
+		return roll;
+	}
+	public void setRoll(String roll) {
+		this.roll = roll;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getClsName() {
+		return clsName;
+	}
+	public void setClsName(String clsName) {
+		this.clsName = clsName;
+	}
+	public String getFatherName() {
+		return fatherName;
+	}
+	public void setFatherName(String fatherName) {
+		this.fatherName = fatherName;
+	}
+	public String getMotherName() {
+		return motherName;
+	}
+	public void setMotherName(String motherName) {
+		this.motherName = motherName;
+	}
+	@Override
+	public String toString() {
+		return "Student [name=" + name + ", roll=" + roll + ", address="
+				+ address + ", clsName=" + clsName + ", fatherName="
+				+ fatherName + ", motherName=" + motherName + "]";
+	}
+	
 }
