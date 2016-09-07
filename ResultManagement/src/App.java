4e65658fc269641136e3d12dd247c0df8bb27974
Siddiqur Rahman <mrahman.vf.ic.bd@gmail.com>
2016-09-07 16:35:10 +0600

    student created in app.

--- ResultManagement/src/App.java
+++ ResultManagement/src/App.java
@@ -1,8 +1,24 @@
 
 public class App {
-
+    private Student[] student;
+    int totalStudent;
+    public App(){
+    	totalStudent=30;
+    	student=new Student[this.totalStudent];
+    }
+    public App(int totalStudent){
+    	this.totalStudent=totalStudent;
+    	student=new Student[this.totalStudent];
+    }
+    public void displayAllStudent(){
+    	for(Student s:this.student){
+    		System.out.println(s);
+    	}
+    }
 	public static void main(String[] args) {
 		System.out.println("Hello world");
+		new App().displayAllStudent();
+		
 	}
 
 }
